06de04c3763952fd6e17d4bfe42a550d5576a806
Kaven Muraleitharan <kavenmuraleitharan0726@gmail.com>
2024-11-15 19:56:46 -0500

    implemented the pretrained model.

--- dermatector.py
+++ dermatector.py
@@ -1,7 +1,11 @@
-# from ultralytics import YOLO
+from ultralytics import YOLO
 import cv2
 
 # Loads the pre trained model. In our case this is YOLOv8
 # model = YOLO('tolov8n.pt')
 
-print(cv2.__version__)
+model = YOLO("yolo11n.pt")  # load an official model
+
+
+# Predict with the model
+results = model(source="0", show=True, conf=0.4, save=True)  # predict on an image
